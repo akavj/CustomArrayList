7dd852c344c932b24252fac559629deb49c3c322
sshubhamv <43710091+sshubhamv@users.noreply.github.com>
2018-09-30 03:24:31 +0530

    Update CustomArrayList.java

--- CutomArrayList/src/CustomArrayList.java
+++ CutomArrayList/src/CustomArrayList.java
@@ -4,6 +4,8 @@ public class CustomArrayList<T> extends ArrayList<T> {
 
 	private static final long serialVersionUID = 1L;
 	ArrayList<T> array = new ArrayList<T>();
+	ArrayList<T> arrayValue = new ArrayList<T>();
+	ArrayList<T> arrayKey = new ArrayList<T>();
 	int size;
 	int index;
 
@@ -48,4 +50,49 @@ public class CustomArrayList<T> extends ArrayList<T> {
 
 	}
 
+	public void addKeyValue(T element1, T element2) {
+
+		arrayKey.add(element1);
+		arrayValue.add(element2);
+
+	}
+
+	public void getAllKeyValue() {
+
+		for (int i = 0; i < (arrayKey.size()) && i < (arrayValue.size()); i++) {
+			System.out.print(arrayKey.get((int) i));
+			System.out.print("->");
+			System.out.println(arrayValue.get((int) i));
+		}
+	}
+
+	public void keyValueGet(int index) {
+
+		System.out.print(arrayKey.get((int) index));
+		System.out.print("->");
+		System.out.println(arrayValue.get((int) index));
+
+	}
+
+	public void removeKey(T key) {
+		int objKey = 0;
+		for (int i = 0; i < arrayKey.size(); i++) {
+			if (arrayKey.get(i).equals(key)) {
+				objKey = i;
+
+			}
+		}
+		arrayKey.remove(objKey);
+		arrayValue.remove(objKey);
+
+	}
+
+	public void removeAllKeyValue() {
+		int i = 0;
+		while (i < arrayKey.size() && i < arrayValue.size()) {
+			arrayKey.remove(i);
+			arrayValue.remove(i);
+		}
+
+	}
 }
